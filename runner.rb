25f49267f8fb52750d3f8272e7f8c97be393e22a
Mark Siemers <mark.siemers@gmail.com>
2017-09-26 09:47:41 -0500

    Initial commit

---
+++ runner.rb
@@ -0,0 +1,24 @@
+class Table
+
+end
+# puts table = Table.new
+
+class Chair
+
+end
+# puts chair = Chair.new
+
+# class Computer
+#
+# end
+# # puts computer = Computer.new
+#
+# class Monitor
+#
+# end
+# # puts monitor = Monitor.new
+#
+# class Kettle
+#
+# end
+# # puts kettle = Kettle.new
